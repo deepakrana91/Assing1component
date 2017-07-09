211fdb4e3705f2e696ef0288535c148689ad77b5
= <=>
2017-07-09 22:44:40 +0530

    First Angular Component

---
+++ src/app/Root/Root.Component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+
+    selector: 'app-rootcom',
+    templateUrl: './Root.Component.html',
+    
+})
+
+export class RootComponent {
+
+
+}
